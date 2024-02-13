639137169eb58a6562b7dad108bd35846de862b0
Nuttawat <kiriblashter@gmail.com>
2024-02-13 10:06:34 +0700

    .

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 const base_url = "http://node58297-noderestnuttawat.proen.app.ruk-com.cloud";
 //const base_url = "http://localhost:3000";
 
-app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
