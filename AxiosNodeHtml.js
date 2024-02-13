0385be1953a004136c0338d04ba877174f8874a6
Nuttawat <kiriblashter@gmail.com>
2024-02-13 09:59:28 +0700

    .

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const path = require("path");
 var bodyParser = require('body-parser');
 
 const base_url = "http://node58297-noderestnuttawat.proen.app.ruk-com.cloud";
 //const base_url = "http://localhost:3000";
 
+app.set("views", path.join(_dirname, "/public/views"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
